7ff3e96e3b5b8d2f403f035728e5d45b7106d04d
miswhiramon <yh7500u@gmail.com>
2020-04-23 03:23:44 +0900

    ""

--- index.js
+++ index.js
@@ -78,21 +78,21 @@ function drawchart(){
     var myPieChart = new Chart(ctx, {
         type: 'pie',
         data: {
-        labels: labels,
-        datasets: [{
-            backgroundColor: [
-                "#BB5179",
-                "#FAFF67",
-                "#58A27C"
-            ],
-            data: data 
-        }]
+          labels: ["A型", "O型", "B型"],
+          datasets: [{
+              backgroundColor: [
+                  "#BB5179",
+                  "#FAFF67",
+                  "#58A27C"
+              ],
+              data: [38, 31, 21]
+          }]
         },
         options: {
-        title: {
+          title: {
             display: true,
-            text: '割合'
+            text: '血液型 割合'
+          }
         }
-        }
-    });
+      });
 }
