89f7425556cee68ee72c4bd609e4a488d743543d
miswhiramon <yh7500u@gmail.com>
2020-04-22 22:40:35 +0900

    js

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ window.addEventListener('message', function(event) {
     }
     kusa.textContent = kusa_counter;
   }, false);
+
+const para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
+function updateName() {
+let name = prompt('名前を入力して下さい');
+para.textContent = 'Player 1: ' + name;
+}
