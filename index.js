8fe41cdcc4183ddf0de6072bd7ae387fc906d0dd
miswhiramon <yh7500u@gmail.com>
2020-04-23 02:53:43 +0900

    graph

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ window.addEventListener('message', function(event) {
     kusa.innerHTML =  "草:" + kusa_counter + "<br>" + "総コメント数" + all_comment_conter 
     + "<br>" + "草割合:" + kusa_counter/all_comment_conter*100 + "%<br>";
 
+    drawchart();
+
     
   }, false);
 
@@ -64,4 +66,29 @@ function onButtonClick(){
     checkword = keyword_form.input_word.value;
     var keyword = document.getElementById("keyword_value");
     keyword.textContent = checkword;
+}
+
+function drawchart(){
+    var ctx = document.getElementById("myPieChart");
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+        labels: ["A型", "O型", "B型", "AB型"],
+        datasets: [{
+            backgroundColor: [
+                "#BB5179",
+                "#FAFF67",
+                "#58A27C",
+                "#3C00FF"
+            ],
+            data: [38, 31, 21, 10]
+        }]
+        },
+        options: {
+        title: {
+            display: true,
+            text: '血液型 割合'
+        }
+        }
+    });
 }
