8a9842c9f9e0517f4da0c1fec7fbb012e46afaba
miswhiramon <yh7500u@gmail.com>
2020-04-28 02:22:30 +0900

    fix

--- index.js
+++ index.js
@@ -179,7 +179,23 @@ function drawBarChart(){
             }
             }]
         },
-        animation:false
+        animation:false,
+
+        onComplete: function () {
+            var chartInstance = this.chart,
+                ctx = chartInstance.ctx;
+            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'bottom';
+
+            this.data.datasets.forEach(function (dataset, i) {
+                var meta = chartInstance.controller.getDatasetMeta(i);
+                meta.data.forEach(function (bar, index) {
+                    var data = dataset.data[index];                            
+                    ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                });
+            });
+        }
         }
     });
 }
