037abc7a61963c3191604c00cf761d94c47a715d
miswhiramon <yh7500u@gmail.com>
2020-05-01 03:31:37 +0900

    fix

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function del_update_id(i){
         //buttonInstance.onclick = new Function("del_update_id("+ label_num_array[j-1] +")");
         //buttonInstance.onclick = new Function(del_update_id(label_num_array[j-1]));
         //buttonInstance.onclick = function() { del_update_id(label_num_array[j-1]); }
-        buttonInstance.onclick = del_update_id(label_num_array[j-1]);
+        buttonInstance.onclick = del_update_id(label_num_array[j-2]);
         //buttonInstance.setAttribute("onclick", ); 
     }
     unable_button();
