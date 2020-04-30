a320bdac9fd0c7e2f1f295f9b2e12b106aaa3470
miswhiramon <yh7500u@gmail.com>
2020-05-01 02:20:21 +0900

    fix

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function del_update_id(i){
 }
 
 function unable_button(){
-    var buttonInstance0 = document.getElementById("Button"+org_choice_label[0]);
+    /*var buttonInstance0 = document.getElementById("Button"+org_choice_label[0]);
     var buttonInstance1 = document.getElementById("Button"+org_choice_label[0]);
     if(num_choice<=2){        
         buttonInstance0.onclick="";
@@ -124,7 +124,7 @@ function unable_button(){
     }else{
         buttonInstance0.onclick= new Function("del_update_id("+ label_num_array[0] +")");
         buttonInstance1.onclick= new Function("del_update_id("+ label_num_array[1] +")");
-    }
+    }*/
 }
 
 function Hankaku2zenkaku(str) {
