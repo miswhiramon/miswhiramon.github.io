7ce068de0deafb4211437f61b106774902ce2dad
miswhiramon <yh7500u@gmail.com>
2020-05-01 00:05:12 +0900

    fix

--- index.js
+++ index.js
@@ -96,8 +96,8 @@ function del_update_id(i){
         instance.id = org_choice_label[j-1];
         var titleInstance = document.getElementById("Title"+ org_choice_label[j]);
         titleInstance.textContent = org_choice_label[j-1];
-        var formInstance = document.getElementById("Form"+org_choice_label[j]);
-        formInstance.placeholder="選択肢"+Hankaku2zenkaku(String(j-1))+"を入力";
+        //var formInstance = document.getElementById("Form"+org_choice_label[j]);
+        //formInstance.placeholder="選択肢"+Hankaku2zenkaku(String(j-1))+"を入力";
     }
 
 }
