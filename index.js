eee18580f7db590f4e342c5ebb4094f40d787706
miswhiramon <yh7500u@gmail.com>
2020-05-01 13:16:59 +0900

    fix

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function addForm(i) {
             <input type="text" placeholder="選択肢`+Hankaku2zenkaku(String(i))+`を入力" id="Form`+ org_choice_label[i] +`">
         </div>
         <div class="col-1 d-flex align-items-center">
-            <div class="batsu d-flex align-items-center" id="Button`+ org_choice_label[i] +`" onclick="del_update_id(`+ i +`)"></div>
+            <div class="batsu d-flex align-items-center" id="Button`+ org_choice_label[i] +`" onclick="del_update_id(`+ String(i) +`)"></div>
         </button>
     </div>
     `);
